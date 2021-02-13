b0ca5b3dd283a7a0afe1ce75e3f69c8d9596507f
priyagupta314-work <75786939+priyagupta314-work@users.noreply.github.com>
2021-02-14 00:11:34 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var likeBtn = document.querySelector("#like-button");
+var likeCtr = document.querySelector("#like-counter"); 
+var likeCount=0;
+console.log(likeCtr);
+likeBtn.addEventListener("click", function likeClickHandler(){
+    likeCount+=1;
+    likeCtr.innerText=likeCount;
+});
+
+
